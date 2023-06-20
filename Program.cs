0cbf9404168bd290df3ce613a68cb467461036fd
huynq1992hpvn <huynq1992hpvn@gmail.com>
2023-06-20 15:13:09 +0700

    lan1

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace timsonhonhat
+{
+    internal class Program
+    {
+        public static int giatrimin(int[]array)
+        {
+            int min = array[0];
+            int index = 0;
+            for(int i = 1; i < array.Length; i++)
+            {
+                if(array[i]<min)
+                {
+                    min = array[i];
+                    index = i;
+                }
+                
+            }
+            return index;
+        }
+        static void Main(string[] args)
+        {
+            int[] bang = { 5, 6, 3, 8, 10 };
+            int index = giatrimin(bang);
+            Console.WriteLine("gia tri nho nhat la: " + bang[index]);
+            Console.ReadKey();
+        }
+    }
+}
